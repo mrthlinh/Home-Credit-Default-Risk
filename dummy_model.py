e9b6955134f63f777d3cfefcb94c5cdb3f0ecb1f
mrthlinh <mrthlinh@gmail.com>
2018-07-06 20:41:24 -0500

    ML dummy Model

---
+++ dummy_model.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  6 15:37:56 2018
+
+@author: mrthl
+"""
+
+# Import library
+import pandas as pd
+
+
+# Read data
+data = pd.read_csv("Data/application_train.csv")
