e4678be3c5df27999893614a6665f895c5b9275c
mrthlinh <mrthlinh@gmail.com>
2018-07-12 22:15:39 -0500

    update

--- dummy_model.py
+++ dummy_model.py
@@ -7,7 +7,53 @@ Created on Fri Jul  6 15:37:56 2018
 
 # Import library
 import pandas as pd
+# sklearn preprocessing for dealing with categorical variables
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler, Imputer
+
+app_train = pd.read_csv("Data/application_train.csv")
+app_test = pd.read_csv("Data/application_test.csv")
+#train = data.copy()
+
+# Create a label encoder object
+le = LabelEncoder()
+le_count = 0
 
 
-# Read data
-data = pd.read_csv("Data/application_train.csv")
+
+# Iterate through the columns
+for col in app_train:
+    if app_train[col].dtype == 'object':
+        # If 2 or fewer unique categories
+        if len(list(app_train[col].unique())) <= 2:
+            # Train on the training data
+            le.fit(app_train[col])
+            # Transform both training and testing data
+            app_train[col] = le.transform(app_train[col])
+            app_test[col] = le.transform(app_test[col])
+            
+            # Keep track of how many columns were label encoded
+            le_count += 1
+            
+print('%d columns were label encoded.' % le_count)
+
+
+# one-hot encoding of categorical variables
+app_train = pd.get_dummies(app_train)
+app_test = pd.get_dummies(app_test)
+
+print('Training Features shape: ', app_train.shape)
+print('Testing Features shape: ', app_test.shape)
+
+
+
+train_labels = app_train['TARGET']
+
+# Align the training and testing data, keep only columns present in both dataframes
+app_train, app_test = app_train.align(app_test, join = 'inner', axis = 1)
+
+# Add the target back in
+app_train['TARGET'] = train_labels
+
+print('Training Features shape: ', app_train.shape)
+print('Testing Features shape: ', app_test.shape)
