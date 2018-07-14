83895934bb6be45fcccdfed9cb7793ced034fa3c
Trang Nguyen <nphoaitrang@gmail.com>
2018-07-14 01:22:06 -0500

    Update EDA
    Next step: try to clean the missing value

--- HomeCreditDefaultRisk_EDA.py
+++ HomeCreditDefaultRisk_EDA.py
@@ -24,6 +24,11 @@ Pmt=pd.read_csv('Data/installments_payments.csv')
 import matplotlib.pyplot as plt
 %matplotlib inline
 
+import cufflinks as cf
+cf.go_offline()
+
+import seaborn as sns
+
 from plotly import __version__
 import plotly.plotly as py
 import plotly.graph_objs as go
@@ -32,9 +37,27 @@ from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 print(__version__)
 
 
+### Missing Value Scan (Will K._Start Here File)
+def missing_values(df):
+    mis_val = df.isnull().sum()
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+    mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+    return mis_val_table_ren_columns
 
 
-### Product Combination
+missing_values(Applicant_Hist)
+missing_values(Bureau)
+
+### 1a Product Combination
+#### Try1
 Data_pie=Applicant_Hist["PRODUCT_COMBINATION"].value_counts()
 fig={
      "data":[{
@@ -65,4 +88,22 @@ fig={
 
 py.iplot(fig, filename='donut')
 
+#### Try2
+#### Note: importing cufflinks and run cf.go_offline() help run the following code
+ProductComb=Applicant_Hist["PRODUCT_COMBINATION"].value_counts()
+df1a = pd.DataFrame({'labels': ProductComb.index,
+                   'values': ProductComb.values
+                  })
+df1a.iplot(kind='pie',labels='labels',values='values', title='Product Combination', hole = 0.5)
 
+### Heatmap for correlation 
+Applicant_Hist_corr=Applicant_Hist.corr()
+
+plt.figure(figsize = (10, 7))
+sns.heatmap(Applicant_Hist_corr, cmap = plt.cm.RdYlBu_r, vmin = -0.25, annot = True, vmax = 0.6)
+plt.title('Historical HC Applicant Feature Correlation Heatmap')
+
+Bureau_corr=Bureau.corr()
+plt.figure(figsize = (10, 7))
+sns.heatmap(Bureau_corr, cmap = plt.cm.RdYlBu_r, vmin = -0.25, annot = True, vmax = 0.6)
+plt.title('Historical Bureau Applicant Feature Correlation Heatmap')
